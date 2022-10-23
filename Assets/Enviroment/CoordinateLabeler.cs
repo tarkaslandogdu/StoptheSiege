dbb13b4e4cacc8fae481b9032a9dd35488b57931
Kedigibibiri <76121293+Kedigibibiri@users.noreply.github.com>
2022-10-23 14:20:47 +0300

    v 0.03
    23-10-2022

--- Assets/Enviroment/CoordinateLabeler.cs
+++ Assets/Enviroment/CoordinateLabeler.cs
@@ -27,6 +27,7 @@ public class CoordinateLabeler : MonoBehaviour
         {
             DisplayCoordinates();
             UpdateObjectName();
+            label.enabled = true;
         }
 
         SetLabelColor();
